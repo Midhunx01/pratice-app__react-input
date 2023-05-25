7c4e76b54f30166f267c6b1fd6f6283882c24e38
Midhun Mohan <midhunxda.123@gmail.com>
2023-05-25 09:30:17 +0530

    added validation for inputs

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -11,7 +11,12 @@ export default function AddUsers() {
 
     function onSubmitHandler(event) {
         event.preventDefault();
-        console.log(username, userAge)
+        if (username.trim().length === 0 || userAge.trim().length === 0  || +userAge < 1){
+            return;
+        }
+        console.log(userAge, username);
+        setUserAge('');
+        setUsername('');
     }
 
     function userInputHandler(event) {
@@ -25,9 +30,9 @@ export default function AddUsers() {
         <Card className='input'>
             <form onSubmit={onSubmitHandler}>
                 <label htmlFor="username">Enter Username</label>
-                <input id="username" type="text" onChange={userInputHandler} />
+                <input id="username" type="text" onChange={userInputHandler} value={username} />
                 <label htmlFor="age">Enter Age (year) </label>
-                <input id="age" type="number" onChange={ageInputHandler} />
+                <input id="age" type="number" onChange={ageInputHandler}  value={userAge}/>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
